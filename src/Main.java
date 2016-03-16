c744b3a2b7483d5c181fe9279c1d3e249cfdbb4e
Mark Wiggans <mmw125@gmail.com>
2016-03-16 00:03:42 -0400

    Added config file because spelling errors annoy users

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -18,8 +22,12 @@ public class Main {
 	private JFrame frame;
 	
 	public Main() {
+		ArrayList<String> ctGuns = new ArrayList<String>();
+		ArrayList<String> tGuns = new ArrayList<String>();
+		
+		
 		Random rand = new Random();
-		frame = new JFrame("Random Gun");
+		frame = new JFrame("Random Guns");
 		frame.getContentPane().setLayout(new BorderLayout());
 		
 		JLabel gun = new JLabel();
@@ -33,54 +41,8 @@ public class Main {
 		ct.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				switch(rand.nextInt(23)) {
-				case 0: gun.setText("P2000");
-					break;
-				case 1: gun.setText("USP-S");
-					break;
-				case 2: gun.setText("P250");
-					break;
-				case 3: gun.setText("Desert Eagle");
-					break;
-				case 4: gun.setText("Dual Brettas");
-					break;
-				case 5: gun.setText("Five SeveN");
-					break;
-				case 6: gun.setText("CZ75 Auto");
-					break;
-				case 7: gun.setText("R8 Revolver");
-					break;
-				case 8: gun.setText("Nova");
-					break;
-				case 9: gun.setText("XM1014");
-					break;
-				case 10: gun.setText("Mag-7");
-					break;
-				case 11: gun.setText("MP9");
-					break;
-				case 12: gun.setText("MP7");
-					break;
-				case 13: gun.setText("UMP-45");
-					break;
-				case 14: gun.setText("PP-Bizon");
-					break;
-				case 15: gun.setText("P90");
-					break;
-				case 16: gun.setText("FAMAS");
-					break;
-				case 17: gun.setText("M4A4");
-					break;
-				case 18: gun.setText("M4A1-S");
-					break;
-				case 19: gun.setText("SSG 08");
-					break;
-				case 20: gun.setText("AUG");
-					break;
-				case 21: gun.setText("AWP");
-					break;
-				case 22: gun.setText("SCAR-20");
-					break;
-				}
+				int i = rand.nextInt(ctGuns.size());
+				gun.setText("CT: " + ctGuns.get(i));
 				frame.revalidate();
 			}
 		});
@@ -90,63 +52,42 @@ public class Main {
 		te.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				int i = rand.nextInt(25);
-				switch(i) {
-				case 0: gun.setText("Glock-18");
-					break;
-				case 1: gun.setText("P250");
-					break;
-				case 2: gun.setText("Desert Eagle");
-					break;
-				case 3: gun.setText("Dual Brettas");
-					break;
-				case 4: gun.setText("Tec-9");
-					break;
-				case 5: gun.setText("CZ75 Auto");
-					break;
-				case 6: gun.setText("R8 Revolver");
-					break;
-				case 7: gun.setText("Nova");
-					break;
-				case 8: gun.setText("XM1014");
-					break;
-				case 9: gun.setText("MP5");
-					break;
-				case 10: gun.setText("Sawed-Off");
-					break;
-				case 11: gun.setText("MAC-10");
-					break;
-				case 12: gun.setText("MP7");
-					break;
-				case 13: gun.setText("UMP-45");
-					break;
-				case 14: gun.setText("PP-Bizon");
-					break;
-				case 15: gun.setText("P90");
-					break;
-				case 16: gun.setText("Galil AR");
-					break;
-				case 17: gun.setText("AK-47");
-					break;
-				case 18: gun.setText("SSG 08");
-					break;
-				case 20: gun.setText("SG 553");
-					break;
-				case 21: gun.setText("AWP");
-					break;
-				case 22: gun.setText("G3SG1");
-					break;
-				case 23: gun.setText("M249");
-					break;
-				case 24: gun.setText("Negev");
-					break;
-				}
+				int i = rand.nextInt(tGuns.size());
+				gun.setText("T: " + tGuns.get(i));
 				frame.revalidate();
 			}
 		});
 		panel.add(te);
 		
-		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("config/config.txt"));
+			String line;
+			int state = 0;
+			while((line = reader.readLine()) != null) {
+				if(line.equals("ct:")) {
+					state = 1;
+				} else if(line.equals("t:")) {
+					state = 2;
+				} else if(line.equals("both:")) {
+					state = 3;
+				} else if(line.trim().equals("")) {
+					//do nothing
+				} else if(state == 1) {
+					ctGuns.add(line);
+				} else if(state == 2) {
+					tGuns.add(line);
+				} else if(state == 3) {
+					ctGuns.add(line);
+					tGuns.add(line);
+				}
+			}
+			reader.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			gun.setText("Could not load config file. It should be in config/config.txt");
+			ct.setEnabled(false);
+			te.setEnabled(false);
+		}
 		
 		frame.getContentPane().add(panel, BorderLayout.WEST);
 		frame.setSize(300, 100);
